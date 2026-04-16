4055ef4936119b2458aaf4effbe20d8899bcb1ae
cassius <cassiuslee@88.com>
2026-04-17 03:36:40 +0800

    feat:删除member-user接口

--- src/main/java/com/citymall/api/module/customer/service/MemberUserQueryService.java
+++ src/main/java/com/citymall/api/module/customer/service/MemberUserQueryService.java
@@ -3,6 +3,9 @@ package com.citymall.api.module.customer.service;
 import com.citymall.api.module.customer.dto.MemberUserInfoQueryDTO;
 import com.citymall.api.module.customer.vo.MemberUserInfoVO;
 
+/**
+ * @author cqkir
+ */
 public interface MemberUserQueryService {
 
     MemberUserInfoVO getMemberUserInfoByUserId(MemberUserInfoQueryDTO queryDTO);
